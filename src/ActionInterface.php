addc135d378ee5dcb8de991bd64facd450bdbc0b
Darren Felton <darren_felton@hotmail.com>
2019-03-05 09:12:40 -0600

    Interface not to enforce constructor

--- src/ActionInterface.php
+++ src/ActionInterface.php
@@ -4,9 +4,5 @@ namespace Kobens\Core;
 
 interface ActionInterface extends \Kobens\Core\Config\RuntimeInterface
 {
-    public function __construct(
-        \Kobens\Core\App\ResourcesInterface $resourcesInterface
-    );
-
     public function execute() : void;
 }
