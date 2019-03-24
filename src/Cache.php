d00d6ff7036f8604c6162c639c91db1e0feb0745
Darren Felton <darren_felton@hotmail.com>
2019-03-24 13:23:04 -0500

    Cheap create dir if not exists
    Will need more work down the road but enough for now.

--- src/Cache.php
+++ src/Cache.php
@@ -3,11 +3,32 @@
 namespace Kobens\Core;
 
 use Zend\Cache\StorageFactory;
+use Kobens\Core\Exception\Exception;
 
 class Cache
 {
     public function getCache()
     {
-        return StorageFactory::factory((new Config())->cache);
+        /** @var \Zend\Config\Config $config */
+        $config = (new Config())->cache;
+        if ((string) $config->adapter->name === 'filesystem') {
+            $this->initCacheDir($config->adapter->options->cache_dir);
+        }
+
+        return StorageFactory::factory($config);
+    }
+
+    protected function initCacheDir(string $dir)
+    {
+        if (is_dir($dir)) {
+            if (!is_readable($dir)) {
+                throw new Exception("Cache Dir \"$dir\" is not readable.");
+            }
+            if (!is_writable($dir)) {
+                throw new Exception("Cache Dir \"$dir\" is not writeable.");
+            }
+        } elseif (!@mkdir($dir, 0700)) {
+            throw new Exception("Failed to make directory \"$dir\"");
+        }
     }
 }
