83a80f562d7582aa54624f498d0f48241e1f1073
Darren Felton <darren.felton@franklincovey.com>
2019-11-24 19:12:30 -0600

    Removed addArgList
    Working on eliminating this over complexity

--- src/Command/Traits/Traits.php
+++ src/Command/Traits/Traits.php
@@ -2,26 +2,10 @@
 
 namespace Kobens\Core\Command\Traits;
 
-use Kobens\Core\Command\Argument\ArgumentInterface;
-use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Output\OutputInterface;
 
 trait Traits
 {
-    protected function addArgList(array $args, Command $command) : void
-    {
-        foreach ($args as $arg) {
-            if (!$arg instanceof ArgumentInterface) {
-                throw new \Exception(\sprintf(
-                    '"%s" only accepts objects of the "%s" interface',
-                    __METHOD__,
-                    ArgumentInterface::class
-                ));
-            }
-            $command->addArgument($arg->getName(), $arg->getMode(), $arg->getDescription(), $arg->getDefault());
-        }
-    }
-
     protected function sleep(OutputInterface $output = null, int $seconds = 5) : void
     {
         if ($seconds <= 0) {
@@ -46,5 +30,4 @@ trait Traits
             $output->write(chr(27).chr(91).'H'.chr(27).chr(91).'J');
         }
     }
-
 }
